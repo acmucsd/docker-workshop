5f701c26d344139721571f063c5867832bacfd99
Nikhil Dange <nsdange@ucsd.edu>
2023-05-10 21:00:57 -0700

    updated code

--- index.js
+++ index.js
@@ -11,13 +11,30 @@ server.use(express.urlencoded({ extended: true }));
 
 // Routes
 router.get('/', (req, res) => {
-    res.send('Hello World');
+    res.status(200).json({ message: 'Hello ACMEats!' })
 });
 
-// Mounting Routes
+router.get('/orders', (req, res) => {
+    const orders = [
+        {
+            id: 1,
+            name: 'Pizza',
+            restaurant: 'NY Pizza Suprema',
+            location: '8th Ave & 31st St'
+        },
+        {
+            id: 2,
+            name: 'Pork Bun',
+            restaurant: 'Mei Lai Wah',
+            location: '64 Bayard St'
+        },
+    ]
+    res.status(200).json({ orders });
+});
+
+
 server.use('/', router);
 
-// Server
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
